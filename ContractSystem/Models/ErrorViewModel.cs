61b501046624aec733743f331c61db22a851ccf6
Busizwe-Coder <busizwe.dl@gmail.com>
2025-09-04 23:57:46 +0200

    Contract system using MVC. First commit.

---
+++ ContractSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContractSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
